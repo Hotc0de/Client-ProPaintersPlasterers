122cad80779214064a28cb1f9f9a064e6e536e80
hotc0de <hotcode22@gmail.com>
2026-03-27 14:46:15 +1300

    eat: implement hero split layout with image and grid structure

--- ProPaintersPlasters/src/sections/Hero/Hero.tsx
+++ ProPaintersPlasters/src/sections/Hero/Hero.tsx
@@ -1,7 +1,8 @@
-import type { Locale } from '../../content/types'
 import { heroContent } from '../../content/home'
-import { getLocalizedValue } from '../../utils/getLocalizedValue'
+import type { Locale } from '../../content/types'
 import './Hero.css'
+
+import heroImage from '../../assets/images/hero/hero.jpg' // or hero/hero.jpg if you move it
 
 type HeroProps = {
   locale: Locale
@@ -10,28 +11,36 @@ type HeroProps = {
 export function Hero({ locale }: HeroProps) {
   return (
     <section className="hero">
-      <div className="hero__content">
-        <p className="hero__eyebrow">
-          {getLocalizedValue(heroContent.eyebrow, locale)}
-        </p>
+      <div className="hero__inner"> {/* 👈 THIS IS NEW */}
+        
+        <div className="hero__content">
+          <p className="hero__eyebrow">
+            {heroContent.eyebrow[locale]}
+          </p>
 
-        <h1 className="hero__title">
-          {getLocalizedValue(heroContent.title, locale)}
-        </h1>
+          <h1 className="hero__title">
+            {heroContent.title[locale]}
+          </h1>
 
-        <p className="hero__description">
-          {getLocalizedValue(heroContent.description, locale)}
-        </p>
+          <p className="hero__description">
+            {heroContent.description[locale]}
+          </p>
 
-        <div className="hero__actions">
-          <a href="#contact" className="hero__button hero__button--primary">
-            {getLocalizedValue(heroContent.primaryButton, locale)}
-          </a>
+          <div className="hero__actions">
+            <a href="#contact" className="hero__button hero__button--primary">
+              {heroContent.primaryButton[locale]}
+            </a>
 
-          <a href="#gallery" className="hero__button hero__button--secondary">
-            {getLocalizedValue(heroContent.secondaryButton, locale)}
-          </a>
+            <a href="#gallery" className="hero__button hero__button--secondary">
+              {heroContent.secondaryButton[locale]}
+            </a>
+          </div>
         </div>
+
+        <div className="hero__image"> {/* 👈 THIS IS NEW */}
+          <img src={heroImage} alt="Painting work" />
+        </div>
+
       </div>
     </section>
   )
